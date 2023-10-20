9120cacc252c6d37e2c17c4179d83e5525f1e817
Satish <slanka1708@gmail.com>
2023-10-19 23:18:02 -0500

    initial commit

---
+++ my-first-demo/my-first-demo/src/test/java/com/example/myfirstdemo/MyFirstDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myfirstdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
